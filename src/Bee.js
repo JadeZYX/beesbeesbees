4c5edeafd50e170c11c41d18be635af2541965d1
Jade Zhang <yxzhang1009@gmail.com>
2022-06-06 14:55:09 -0700

    finish

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,23 @@
-var Bee = function() {
-};
+var Bee = function(   ) {
 
+  Grub.call(this);
+  this.age =5;
+  this.color = "yellow";
+  this.job = "keep on growing";
+};
+Bee.prototype = Object.create(Grub.prototype);//this destroys the default Bee.prototype.constructor
+Bee.prototype.constructor = Bee;//so we need to recreate it
+//Bee.prototype = Grub.prototype//BAD:This makes them the same exact obj
+//Bee.prototype = new Grub()//BAD:Creates a redundant instance
+
+/*
+class Bee extends Grub{
+  constructor(){
+    super();
+    this.age = 5;
+    this.color = "yellow";
+    this.job = "keep on growing";
+  }
+  fly(){}
+}
+*/
--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,17 @@
 var Grub = function() {
+  //this === the object that will be created
+  this.age = 0;
+  this.color = "pink";
+  this.food = "jelly";
+  this.eatTime = 0;
 };
+Grub.prototype.eat = function(){
+  this.eatTime++;
+}
 
+var eric = new Grub();
+//the new keyword
+//-create a new object instance
+//-connects this new object to <Constructor>.prototype using prototypal delegation
+//-run the constructor func in the context of the new object
+//-gives you the new object to use
